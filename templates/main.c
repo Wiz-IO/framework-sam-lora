210b84526ab2db7c118c1854744ea10abbe94762
Georgi Angelov <the.wizarda@gmail.com>
2019-11-09 17:32:45 +0200

    Add files via upload

---
+++ templates/main.c
@@ -0,0 +1,17 @@
+/*  
+    Microchip Atmel ATSAM3x 2019 Georgi Angelov
+        http://www.wizio.eu/    
+        https://github.com/Wiz-IO/platform-sam-lora
+
+    OPEN: 'platformio.ini' and edit your settings
+ */
+
+#include <stdint.h>
+#include <samr34.h>
+
+int main(void) {
+    // SETUP
+    while(1) {
+        // LOOP
+    }
+}
