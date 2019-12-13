4a9c626cc0a47fd72b39dfccd8e65dbc163e0d5a
Georgi Angelov <the.wizarda@gmail.com>
2019-12-13 12:35:02 +0200

    RTC store/restore backup values

--- examples/arduino_rtc_psm/src/main.cpp
+++ examples/arduino_rtc_psm/src/main.cpp
@@ -1,25 +1,41 @@
 #include <Arduino.h>
 #include <RF.h>
 #include <RTC.h>
-RTCClass rtc(1);
+RTCClass rtc;
+
+uint32_t psm_counter; // test value for rtc backup
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("\nMicrochip ATSAMR34 Xplained Pro 2019 Georgi Angelov");
   RF.end(); // put RF to sleep
-  Serial.printf("Reset %d\n", get_reset_cause());
+
+  int c = get_reset_cause();
+  Serial.printf("Reset %d\n", c);
+  if (RESET_CAUSE_BACKUP == c)
+  {
+    rtc.get_backup(1 /*max 4 values*/, &psm_counter); // restore from rtc ram, after rtc.begin() value is not valid
+    Serial.printf("PSM counter = %d\n", psm_counter);
+  }
+  else
+  {
+    Serial.printf("PSM counter Reset\n");
+    psm_counter = 0;
+  }
+
+  rtc.begin();
   pinMode(LED_G, OUTPUT);
   digitalWrite(LED_G, LED_OFF);
 }
 
 void loop()
 {
-  // blink led 4 seconds, then sleep for 20 seconds
-  digitalToggle(LED_G, 100);
+  digitalToggle(LED_G, 100); // blink led 4 seconds, then sleep for 20 seconds
   if (seconds() > 4)
   {
-    digitalWrite(LED_G, LED_OFF);
-    rtc.sleep_wakeup(20); // sleep ... wakeup after 20 seconds
+    digitalWrite(LED_G, LED_OFF);                      // led off
+    rtc.set_backup(1 /*max 4 values*/, ++psm_counter); // save to rtc ram
+    rtc.sleep_wakeup(20);                              // sleep ... wakeup after 20 seconds
   }
 }
